9e550360a031368a6481f00258c61100ccb88151
pspanishetti <praveenpanishetti@gmail.com>
2020-01-12 20:15:24 +0530

    Added Comments

--- app/src/main/java/in/codeworld/swipegesturedetection/MainActivity.java
+++ app/src/main/java/in/codeworld/swipegesturedetection/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private View rootLayout;
     private TextView resultText;
+
     private SwipeGestureDetector swipeGestureDetector;
     private GestureDetectorCompat gestureDetectorCompat;
 
@@ -28,24 +29,26 @@ public class MainActivity extends AppCompatActivity {
         rootLayout= findViewById(R.id.root_layout);
         resultText=findViewById(R.id.my_text_view);
 
+        //Assign the swipeGestureDetector by creating new instance
+
         swipeGestureDetector=new SwipeGestureDetector(new SwipeActions() {
             @Override
             public void onSwipeLeft() {
-              //  Toast.makeText(MainActivity.this, "Swiped Left", Toast.LENGTH_SHORT).show();
+                //Write The actions need to be performed when Swiped Left here
                 resultText.setText("Swiped Left");
                 rootLayout.setBackgroundColor(getResources().getColor(R.color.colorLeft));
             }
 
             @Override
             public void onSwipeRight() {
-               // Toast.makeText(MainActivity.this, "Swiped Right", Toast.LENGTH_SHORT).show();
+                //Write The actions need to be performed when Swiped Right here
                 resultText.setText("Swiped Right");
                 rootLayout.setBackgroundColor(getResources().getColor(R.color.colorRight));
             }
 
             @Override
             public void onSwipeUp() {
-               // Toast.makeText(MainActivity.this, "Swiped Up", Toast.LENGTH_SHORT).show();
+                //Write The actions need to be performed when Swiped up here
                 resultText.setText("Swiped Up");
                 rootLayout.setBackgroundColor(getResources().getColor(R.color.colorUp));
 
@@ -53,19 +56,22 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onSwipeDown() {
-               // Toast.makeText(MainActivity.this, "Swiped Down", Toast.LENGTH_SHORT).show();
+                //Write The actions need to be performed when Swiped down here
                 resultText.setText("Swiped Down");
                 rootLayout.setBackgroundColor(getResources().getColor(R.color.colorDown));
             }
         });
 
 
+        //Assign GestureDetectorCompat instance by passing swipeGestureDetector instance
         gestureDetectorCompat = new GestureDetectorCompat(getApplicationContext(), swipeGestureDetector);
 
+        //Set onTouchListener on rootLayout
         rootLayout.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 gestureDetectorCompat.onTouchEvent(event);
+                //Make sure it returns true because event needs to be consumed in the end
                 return true;
             }
         });
